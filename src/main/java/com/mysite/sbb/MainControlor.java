e6dbfc8b94a0136951e10ed71071360234e9a34e
jkkjkhh <jkkjkhh@gmail.com>
2022-07-04 11:38:34 +0900

    기본 셋팅

---
+++ src/main/java/com/mysite/sbb/MainControlor.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainControlor {
+
+  @RequestMapping("/sbb")
+  @ResponseBody
+  public String index() {
+    return "ㅎㅇㅎㅇ";
+  }
+
+}
